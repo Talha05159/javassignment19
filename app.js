1c0bb65ec8b34e74d2d5e1e938940ba8c56eac15
Talha05159 <mtajig05@gmail.com>
2020-06-15 11:08:42 -0700

    ready to commit

---
+++ app.js
@@ -0,0 +1,294 @@
+//CHAPTER # 1
+alert ("Error!Please enter a valid password.")
+
+alert ("Welcome to JS Land... Happy Coding " )
+
+alert ("Welcome to JS Land")
+
+alert ("Happy Coding!" )
+
+alert ("Hello... I can run JS through my web browser's console")
+
+//CHAPTER # 2
+
+var username = a;
+
+var myName = ("TALHA GODIL")
+
+var message = ("Hello World!")
+
+alert (message)
+
+var a = ("Amaan Naeem")
+alert (a);
+
+var b = ("17 years old")
+alert (b);
+
+var c = ("BE CIVIL ENGINEERING")
+alert (c);
+
+var email = ("My email address is mtajig05@gmail.com")
+alert (email);
+
+var h = ("I am trying to learn from the Book A smarter way to learn JavaScript")
+alert (h);
+
+var n = ("Yah! I can write HTML content through JavaScript " + "<br>")
+document.write (n);
+
+
+
+//CHAPTER # 3
+
+var age = ("I am 20 years old")
+alert (age);
+
+var a = ("You have visited this site 26 times" )
+alert (a);
+
+var birthyear = ("My birth is 2000" + "<br>")
+document.write (birthyear);
+
+
+var b = ("Data type of my declared variable is number" + "<br>")
+document.write (b);
+
+var c = ("Muhammad Sajid ordered 7 Denims on ATTIRE WEARS" + "<br>")
+document.write (c);
+
+// CHAPTER # 4
+
+
+var a = 5;
+var b = 10;
+var c = (a+b);
+
+console.log (c)
+
+var ta = ("Rules of naming JS Variable"+ "<br>" + "<br>")
+document.write (ta)
+
+
+var ti = ("Variable names can only contain,numbers,$ and _.For example : $my_1stVariable" + "<br>")
+document.write (ti)
+
+var tu = ("Variable must begin with a letter,$ or _.For Example : $name,_name or name " + "<br>")
+document.write (tu)
+
+var to = ("Variable names are case sensitive" + "<br>")
+document.write (to)
+
+var te = ("Variable names should not be JS Keywords" + "<br>")
+
+
+//CHAPTER # 5
+
+var t = 5;
+var r = 6;
+var q = (t+r);
+document.write ("Sum of 5 and 6 is" + q + "<br>")
+
+var ps = 5;
+var rs = 6;
+var qs = (ps - rs);
+document.write ("Sum of 5 and 6 " + qs + "<br>")
+
+var pm = 5;
+var rm = 6;
+var qm = (pm * rm);
+document.write ("Sum of 5 and 6 is " + qm + "<br>")
+
+var pd = 5;
+var rd = 6;
+var qd = (pd/rd);
+document.write ("Sum of 5 and 6 is" + qd + "<br>")
+
+var s = ("Value after declaration is undefined" + "<br>")
+document.write (s);
+
+var s = 5;
+document.write ("Intial value is" + s + "<br>")
+
+
+
+
+
+
+
+//CHAPTER # 6 - 9
+
+var a = 10;
+var c = ("The value of a is " + a + "<br>")
+document.write (c)
+
+var b =  a + ++a ;
+var d =  ("Now the value of a is" + b + "<br>")
+document.write (d)
+
+var h = a + a++ ;
+var g = (" Now The value of a is" + h + "<br>")
+document.write (g)
+
+var x = a + --a;
+var y = ("Now the value of a is"+ x + "<br>")
+document.write (y)
+  
+var j = 2;
+var k = 1;
+var l = --j - --k  + ++k  + k--;
+document.write (l + "<br>")
+
+var name = prompt ("Enter Your Name")
+{
+    alert ("Welcome Here")
+}
+
+var i = 5*1;
+for(i = 1; i <= 10 ; i++)
+document.write (i)
+
+
+
+
+
+
+
+
+
+
+
+// CHAPTER # 9 - 11
+
+var city = prompt ("Enter City Name")
+ { 
+    alert = ("Welcome To City Of Lights")
+}
+
+
+
+var gender = prompt ("Enter Your Gender")
+if (gender = "male"){
+   alert ("Good Morning Sir")
+}
+ else
+  {
+    alert ("Good Morning Maam")
+}
+
+
+var e = prompt ("Road Traffic Signal")
+if (e = "red"){
+    alert ("Must Stop")
+}else if (e ="yellow" ) {
+    alert ("Ready To Move")
+}else if (e = "green") {
+    alert ("Move Now")
+}
+
+var o  = prompt ("Remaining Fuel In Litres")
+if (o < 0.25 ){
+alert ("Please Refill Your Fuel")
+}else {
+    alert ("Your Fuel Is Okay")
+}
+
+//a
+var a = 4;
+if(c++ === 5){
+    alert ("given condition for variable b is true")
+}
+
+//b 
+var b = 82 ;
+if(b++ === 83){
+    alert ("given condition for b is true")
+}
+
+//c
+var c = 12;
+if(c++ === 13){
+    alert ("condition 1 is true")
+}
+if(c === 13){
+    alert ("condition 2 is true")
+}
+if(++c < 14){
+    alert ("condition 3 is truemmmmmm")
+}
+if(c === 14 ){
+    alert ("condition 4 is true")
+}
+
+//d 
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost =materialCost + laborCost;
+if(totalCost === materialCost + laborCost){
+    alert ("The cost equals")
+}
+
+//e 
+if(true){
+    alert ("True");
+}
+//if (false){
+ //   alert ("false");
+//}
+ 
+//f
+
+//if("car" < "cat"){
+  //  alert ("car is smaller than cat")
+//}
+
+
+//CHAPTER # 17 - 20
+
+var mut = 5;
+for(mut = 5; mut <= 100 ; mut + 5)
+document.write (mut);
+
+//var q= [[]];
+
+//var v =  Array [["0,1,2,3" + "<br>" ],["1,0,1,2" + "<br>"],["2,1,0,1" + "<br>"]];
+//document.write (v);
+
+var ui = 1;
+for(ui=1 ; ui <= 10; ui++);
+console.log (ui);
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
